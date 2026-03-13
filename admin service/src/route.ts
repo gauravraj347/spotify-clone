d11656daa13060f35de3a418e308fb703b4be53f
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-13 21:48:38 +0530

    add thumbnail

--- admin service/src/route.ts
+++ admin service/src/route.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express"
-import { addAlbum, addSong } from "./constroller.js";
+import { addAlbum, addSong, addThumbnail } from "./constroller.js";
 import uploadFile, { isAuth } from "./middleware.js";
 
 const router = express.Router();
 
 router.post("/album/new", isAuth, uploadFile, addAlbum);
 router.post("/song/new", isAuth, uploadFile, addSong);
+router.post("/song/:id", isAuth, uploadFile, addThumbnail);
 
 export default router;
