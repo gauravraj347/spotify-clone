a468d0ab468a33c3151b873163287275da39a210
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-13 21:40:54 +0530

    add song

--- admin service/src/route.ts
+++ admin service/src/route.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express"
-import { addAlbum } from "./constroller.js";
+import { addAlbum, addSong } from "./constroller.js";
 import uploadFile, { isAuth } from "./middleware.js";
 
 const router = express.Router();
 
 router.post("/album/new", isAuth, uploadFile, addAlbum);
+router.post("/song/new", isAuth, uploadFile, addSong);
 
 export default router;
