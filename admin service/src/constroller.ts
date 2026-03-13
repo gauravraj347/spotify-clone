138db84097b342fc3a4df21ee27a2c79bd489190
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-13 21:56:14 +0530

    delete album

--- admin service/src/constroller.ts
+++ admin service/src/constroller.ts
@@ -151,3 +151,31 @@ export const addThumbnail = TryCatch(async (req: AuthencatedRequest, res) => {
     song: result[0],
   });
 });
+
+export const deleteAlbum = TryCatch(async (req: AuthencatedRequest, res) => {
+  if (req.user?.role !== "admin") {
+    res.status(401).json({
+      message: "You are not admin",
+    });
+    return;
+  }
+
+  const { id } = req.params;
+
+  const isAlbum = await sql`SELECT * FROM albums WHERE id = ${id}`;
+
+  if (isAlbum.length === 0) {
+    res.status(404).json({
+      message: "No album with this id",
+    });
+    return;
+  }
+
+  await sql`DELETE FROM songs WHERE album_id = ${id}`;
+
+  await sql`DELETE FROM albums WHERE id = ${id}`;
+
+  res.json({
+    message: "Album deleted successfully",
+  });
+});
--- admin service/src/route.ts
+++ admin service/src/route.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express"
-import { addAlbum, addSong, addThumbnail } from "./constroller.js";
+import { addAlbum, addSong, addThumbnail, deleteAlbum } from "./constroller.js";
 import uploadFile, { isAuth } from "./middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/album/new", isAuth, uploadFile, addAlbum);
 router.post("/song/new", isAuth, uploadFile, addSong);
 router.post("/song/:id", isAuth, uploadFile, addThumbnail);
+router.delete("/album/:id", isAuth, deleteAlbum);
 
 export default router;
