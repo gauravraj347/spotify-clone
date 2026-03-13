5b273fe681b04921a95bd67bfb81ddb419b3b28a
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-13 13:42:14 +0530

    Database neon postgresql setup

--- admin service/src/index.ts
+++ admin service/src/index.ts
@@ -1,11 +1,45 @@
 import express from "express"
 import dotenv from "dotenv"
+import { sql } from "./config/db.js";
 
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 7000
-app.listen(PORT , ()=>{
-    console.log(`Sever running on PORT ${PORT}`)
-})
+
+async function initDB() {
+  try {
+    await sql`
+        CREATE TABLE IF NOT EXISTS albums(
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(255) NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          thumbnail VARCHAR(255) NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+
+    await sql`
+        CREATE TABLE IF NOT EXISTS songs(
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(255) NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          thumbnail VARCHAR(255),
+          audio VARCHAR(255) NOT NULL,
+          album_id INTEGER REFERENCES albums(id) ON DELETE SET NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDb", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
+});
