6be1ba86b78a443097526567257427d0b2b3b84c
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-14 12:57:19 +0530

    add redis

--- admin service/src/index.ts
+++ admin service/src/index.ts
@@ -3,8 +3,22 @@ import dotenv from "dotenv"
 import { sql } from "./config/db.js";
 import adminRoutes from "./route.js"
 import { v2 as cloudinary } from "cloudinary";
+import { createClient } from "redis";
 
 dotenv.config()
+
+export const redisClient = createClient({
+  password: process.env.Redis_Password ||"",
+  socket: {
+    host: "redis-11661.crce281.ap-south-1-3.ec2.cloud.redislabs.com",
+    port: 11661,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
 
 cloudinary.config({
   cloud_name: process.env.Cloud_Name!,
