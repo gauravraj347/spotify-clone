1a1356882d37cfd4a020361e45c049fbe7840e0f
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-14 12:46:12 +0530

    add redis

--- song service/src/TryCatch.ts
+++ song service/src/TryCatch.ts
@@ -1,4 +1,4 @@
-import type { NextFunction, Request, RequestHandler, Response } from "express";
+import type{ NextFunction, Request, RequestHandler, Response } from "express";
 
 const TryCatch = (handler: RequestHandler): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
--- song service/src/index.ts
+++ song service/src/index.ts
@@ -1,8 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
 import songRoutes from "./route.js"
+import { createClient } from "redis";
  
 dotenv.config()
+
+export const redisClient = createClient({
+  password: process.env.Redis_Password ||"",
+  socket: {
+    host: "redis-11661.crce281.ap-south-1-3.ec2.cloud.redislabs.com",
+    port: 11661,
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
 
 const app = express();
 
