008dd9195a1437c822e09cc74ba41e066dedcf50
gauravraj347 <gauravrajmdk3476@gmail.com>
2026-03-14 13:08:34 +0530

    add cors

--- user service/src/index.ts
+++ user service/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import mongoose from 'mongoose';
 import userRoutes from './router.js';
+import cors from "cors"
 dotenv.config();
 
 const app = express();
@@ -21,6 +22,9 @@ const connectDB = async ()=>{
 }
 
 app.use(express.json());
+
+app.use(cors());
+
 app.use('/api/v1',userRoutes)
 
 const PORT = process.env.PORT || 3000;
